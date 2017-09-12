ba369981050b9febedd458c40af5503765c6c288
Reena Patil <reena.patil@outlook.com>
2017-09-12 14:34:11 +1000

    modified boggle_board.rb

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,13 +1,30 @@
 class BoggleBoard
   def initialize
+    #how do we describe it
+    #creating an array of 16 underscores
+    @spaces = Array.new(16, "_")
   end
-
+#what does it do
   def shake!
   end
 
   # Defining to_s on an object controls how the object is
   # represented as a string, e.g., when you pass it to puts
   def to_s
-    "omg what is this? try printing me."
+    # a new string for theoutput
+    output_string = String.new
+    #making a variable to mess with so i dont screw up my instance variable
+    letter_array = @spaces
+
+    4.times do
+       output_string << letter_array.shift(4).join('') + "\n"
+    end
+    
+    output_string
+
   end
+
 end
+
+board = BoggleBoard.new
+puts board
