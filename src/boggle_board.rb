a4fc554b69d3e5509e79913e0cca4ed5ee46309c
Reena Patil <reena.patil@outlook.com>
2017-09-13 10:51:24 +1000

    added new file

--- src/boggle_board.rb
+++ src/boggle_board.rb
@@ -1,33 +1,76 @@
-class BoggleBoard
+# class BoggleBoard
+#
+#   def initialize
+#     @spaces = Array.new(16,"_")
+#     @alphabet =('A'..'Z'). to_a
+#   end
+#
+#   def shake!
+#     #take spaces
+#     #replace them with random letters
+#     #spit it into our board#call the shake method
+#     @spaces.map! do | space |
+#     space = @alphabet.shuffle.take(1)
+#     end
+#
+#   end
+#
+#  # Defining to_s on an object controls how the object is
+#   # represented as a string, e.g., when you pass it to puts
+#   def to_s
+#     # output_string = String.new
+#     # letter_array = @spaces
+#     #
+#     # 4.times do
+#     #   output_string << letter_array.shift(4).join('') + "\n"
+#     # end
+#     # puts output_string
+#   end
+# end
+#
+# board = BoggleBoard.new
+# puts board.to_s
+# puts board.shake!
 
-  def initialize
-    @spaces = Array.new(16,"_")
-    @alphabet =('A'..'Z'). to_a
-  end
 
-  def shake!
-    #take spaces
-    #replace them with random letters
-    #spit it into our board#call the shake method
-    @spaces.map! do | space |
-    space = @alphabet.shuffle.take(1)
+
+class Board
+  def initialize(state)
+    @xmax = state.size
+    @ymax = state[0].size
+
+    state.each do |row|
+      raise "Board rows are of varying lengths" if row.size != @ymax
     end
 
+    @state = state
   end
 
- # Defining to_s on an object controls how the object is
-  # represented as a string, e.g., when you pass it to puts
-  def to_s
-    # output_string = String.new
-    # letter_array = @spaces
-    #
-    # 4.times do
-    #   output_string << letter_array.shift(4).join('') + "\n"
-    # end
-    # puts output_string
+  def [](a, b)
+    @state[a][b]
+  end
+
+  def each()
+    for x in 0...@xmax
+      for y in 0...@ymax
+        yield x, y
+      end
+    end
+  end
+
+  def neighbor_iter(x, y)
+    temp = @state[x][y]
+    @state[x][y] = nil
+
+    xrange = ([x-1, 0].max)..([x+1, @xmax-1].min)
+    yrange = ([y-1, 0].max)..([y+1, @ymax-1].min)
+
+    for i in xrange
+      for j in yrange
+        yield i, j if @state[i][j]
+      end
+    end
+
+    @state[x][y] = temp
   end
 end
-
-board = BoggleBoard.new
-puts board.to_s
-puts board.shake!
--- src/boggle_board_spec.rb
+++ src/boggle_board_spec.rb
@@ -1,48 +1,84 @@
-require_relative 'boggle_board'
+# require_relative 'boggle_board'
+#
+# describe "Boggle Board - Release 0" do
+#
+#   let(:board) { BoggleBoard.new }
+#
+#   it "can be shaken" do
+#     expect{board.shake!}.not_to raise_error
+#   end
+#
+#   it 'has a printable representation of 4 underscores in each of 4 rows on separate lines BEFORE it is shaken' do
+#     expect(board.to_s).to match(/^_{4}${4}/)
+#   end
+#
+#   it 'has a printable representation of 4 letters in each of 4 rows on separate lines AFTER it is shaken' do
+#     board.shake!
+#     expect(board.to_s).to match(/^[A-Z]{4}${4}/)
+#   end
+#
+#   it 'has the same state every time to_s is called' do
+#     first_string = board.to_s
+#     second_string = board.to_s
+#     expect(first_string).to eq second_string
+#   end
+#
+#   it 'has a different state after shake is called' do
+#     first_string = board.to_s
+#     board.shake!
+#     second_string = board.to_s
+#     expect(first_string).not_to eq second_string
+#   end
+#
+#   it 'has 4 rows' do
+#     expect(board.shake!.length).to eq(4)
+#   end
+#
+#   it 'has 4 rows with 4 letters each' do
+#     expect(board.shake!.all? {|row| row.length == 4}).to be_truthy
+#   end
+# end
+#
+# describe "Boggle Board - Release 1" do
+#   pending "Write your own tests here"
+# end
+#
+# describe "Boggle Board - Release 2" do
+#   pending "Write your own tests here"
+# end
 
-describe "Boggle Board - Release 0" do
+def search(board, pos, dict, prefix)
+  words = []
 
-  let(:board) { BoggleBoard.new }
-
-  it "can be shaken" do
-    expect{board.shake!}.not_to raise_error
+  if dict.is_word?
+    words << prefix
   end
 
-  it 'has a printable representation of 4 underscores in each of 4 rows on separate lines BEFORE it is shaken' do
-    expect(board.to_s).to match(/^_{4}${4}/)
+  board.neighbor_iter(*pos) do |x, y|
+    if dict.keys.member? board[x, y]
+      recur = search(board, [x, y], dict[board[x, y]], prefix+board[x, y])
+      words.concat(recur)
+    end
   end
 
-  it 'has a printable representation of 4 letters in each of 4 rows on separate lines AFTER it is shaken' do
-    board.shake!
-    expect(board.to_s).to match(/^[A-Z]{4}${4}/)
-  end
-
-  it 'has the same state every time to_s is called' do
-    first_string = board.to_s
-    second_string = board.to_s
-    expect(first_string).to eq second_string
-  end
-
-  it 'has a different state after shake is called' do
-    first_string = board.to_s
-    board.shake!
-    second_string = board.to_s
-    expect(first_string).not_to eq second_string
-  end
-
-  it 'has 4 rows' do
-    expect(board.shake!.length).to eq(4)
-  end
-
-  it 'has 4 rows with 4 letters each' do
-    expect(board.shake!.all? {|row| row.length == 4}).to be_truthy
-  end
+  words
 end
 
-describe "Boggle Board - Release 1" do
-  pending "Write your own tests here"
+
+board = Board.new($<.readlines.map {|line| line.scan(/\w/)})
+
+dict = Trie.new()
+File.open('/usr/share/dict/words').each do |line|
+  dict.addword(line.chomp.downcase)
 end
 
-describe "Boggle Board - Release 2" do
-  pending "Write your own tests here"
+
+results = []
+board.each do |x, y|
+  results.concat(search(board, [x, y], dict[board[x, y]], board[x, y]))
 end
+
+results.uniq!
+results.sort! {|a, b| a.size - b.size}
+
+puts results
